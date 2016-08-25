8d6e46ad3f30624f7dfea4dd804f00655e75f3a9
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-08-25 12:54:02 -0400

    create cats

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'rack-cors'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'pry-rails'
 end
 
 group :development do
--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -11,6 +11,21 @@ module Api
         render json: Cat.find(params[:id])
         # render json: Cat.find(params[:id]), include: ['hobbies']
       end
+
+      def create
+        cat = Cat.new(cat_params)
+        if cat.save
+          render json: cat
+        else
+          render json: {status: 500, err: 'cat could not be saved.'}
+        end
+      end
+
+      private
+
+        def cat_params
+          params.require(:cat).permit(:name, :breed, :temperament)
+        end
     end
   end
 end
