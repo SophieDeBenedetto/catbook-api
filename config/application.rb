b2996f3d35c0db8705cc355f9d4ac1c25113f76d
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-09-13 14:38:23 -0400

    add auth to initializer

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
+require "./lib"
 
 
 # Require the gems listed in Gemfile, including any gems
