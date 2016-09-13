d91612e18b3dc491511775b32a508ac4ed8ddf9e
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-09-13 10:37:30 -0400

    fleshed out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
-c1 = Cat.create(name: "Moe", breed: "Tabby", weight: "fat")
-c2 = Cat.create(name: "Ciprian", breed: "Calico", weight: "skinny")
+c1 = Cat.create(name: "Moe", breed: "Tabby", weight: "fat", temperament: "protective")
+c2 = Cat.create(name: "Ciprian", breed: "Calico", weight: "skinny", temperament: "friendly")
+c3 = Cat.create(name: "Mini", breed: "Tabby", weight: "large", temperament: "sweet")
+c4 = Cat.create(name: "Sparky", breed: "dog", weight: "average", temperament: "confused")
+c5 = Cat.create(name: "Nona", breed: "Calico", weight: "a bit fat", temperament: "mean and rude")
 
 h1 = Hobby.create(name: "eating")
 h2 = Hobby.create(name: "playing")
