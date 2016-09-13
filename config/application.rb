41065ededefccf80a38026eeae93aba47c01c76d
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-09-13 14:39:48 -0400

    add lib to eager load path

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
-require "./lib"
 
 
 # Require the gems listed in Gemfile, including any gems
@@ -28,6 +27,7 @@ module CatBook
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.autoload_paths << Rails.root.join('lib') 
+    config.eager_load_paths << Rails.root.join('lib') 
     config.api_only = true
   end
 end
